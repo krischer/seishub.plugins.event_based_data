06094bf382d74551f950d51eecbdeb1a4e076077
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2012-12-07 17:17:29 +0100

    Finished waveform indexing tests.

--- seishub/plugins/event_based_data/tests/test_waveform.py
+++ seishub/plugins/event_based_data/tests/test_waveform.py
@@ -105,6 +105,10 @@ class WaveformTestCase(EventBasedDataTestCase):
         self.assertTrue(waveform.filepath is filepath_object)
         self.assertTrue(waveform.channel is channel)
 
+        # Also check that it has been uploaded to the correct directory.
+        self.assertEqual(os.listdir(os.path.join(self.tempdir, "waveform_data",
+            "PM", "PFVI")), ["PM.PFVI..BHE-2012_8_27_4"])
+
     def test_settingSyntheticsFlagMarksFileAsSynthetic(self):
         """
         Simple test that checks that a file uploaded with synthetic=true marks
@@ -175,6 +179,71 @@ class WaveformTestCase(EventBasedDataTestCase):
         self.assertRaises(InvalidParameterError, self._send_request, "POST",
             "/event_based_data/waveform", None,
             {"event": "example_event", "index_file": self.data_dir})
+
+    def test_indexWaveformFile(self):
+        """
+        Index a waveform file. Most things should be exactly the same as for
+        the normal upload case. Only the file should of course not be copied.
+
+        This test is almost identical to test_uploadingSingleSACFile().
+        """
+        # Upload an event to be able to refer to one.
+        self._upload_event()
+        waveform_file = os.path.join(self.data_dir, "dis.PFVI..BHE")
+        data = self._send_request("POST",
+            "/event_based_data/waveform", None,
+            {"event": "example_event", "index_file": waveform_file})
+
+        # Get the filepath object. Database should only contain one!
+        session = self.env.db.session(bind=self.env.db.engine)
+        filepath_object = session.query(FilepathObject).one()
+
+        # The file is not managed by SeisHub.
+        self.assertEqual(filepath_object.is_managed_by_seishub, False)
+        # has no associated origin file.
+        self.assertEqual(filepath_object.file_origin_resource_id, None)
+        # The filepath should be identical to the actual file in this case.
+        self.assertEqual(filepath_object.filepath,
+            os.path.abspath(waveform_file))
+        # The size should be identical to the stored size.
+        self.assertEqual(filepath_object.size, os.path.getsize(waveform_file))
+
+        # Now check the databases. Should contain exactly one entry in the
+        # station table, one in the channels table and one in the metadata
+        # table.
+        station = session.query(StationObject).one()
+        channel = session.query(ChannelObject).one()
+        # Assert the station information
+        self.assertEqual(station.network, "PM")
+        self.assertEqual(station.station, "PFVI")
+        # SAC files do contain coordinates.
+        self.assertAlmostEqual(station.latitude, 37.132832, 5)
+        self.assertAlmostEqual(station.longitude, -8.826829, 5)
+        self.assertAlmostEqual(station.elevation_in_m, 189.0, 5)
+        self.assertAlmostEqual(station.local_depth_in_m, 3.0, 5)
+        # Assert the channel information.
+        self.assertEqual(channel.location, "")
+        self.assertEqual(channel.channel, "BHE")
+        self.assertTrue(channel.station is station)
+
+        # Check the waveform channel
+        waveform = session.query(WaveformChannelObject).one()
+        self.assertEqual(waveform.starttime,
+            datetime.datetime(2012, 8, 27, 4, 43, 56, 35004))
+        self.assertEqual(waveform.endtime,
+            datetime.datetime(2012, 8, 27, 5, 48, 55, 985004))
+        self.assertAlmostEqual(waveform.sampling_rate, 20.0)
+        self.assertEqual(waveform.format, "SAC")
+        self.assertEqual(waveform.is_synthetic, False)
+        # Currently has no associate metadata and processing information
+        self.assertEqual(waveform.metadata_resource_id, None)
+        self.assertEqual(waveform.processing_history_resource_id, None)
+        # Check the relationships.
+        self.assertTrue(waveform.filepath is filepath_object)
+        self.assertTrue(waveform.channel is channel)
+
+        # Also check that the actual data directory has no entries!
+        self.assertEqual(os.listdir(self.tempdir), [])
 
 
 def suite():
